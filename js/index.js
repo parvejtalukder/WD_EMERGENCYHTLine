ed2aa74984ced5edaea11c8946ecce1ebcf4f19a
parvejtalukder <mail@parvejhusentalukder.com>
2025-08-28 16:37:42 +0600

    Update: History Part Ongoing

--- js/index.js
+++ js/index.js
@@ -20,6 +20,7 @@ function getElement(id) {
 
 function heartClicked(id) {
     getElement(id).addEventListener("click", function(event) {
+        event.stopPropagation();
         heartCount++;
         getElement("heartCount").innerText = heartCount;
     });
@@ -27,6 +28,7 @@ function heartClicked(id) {
 
 function copyClicked(id, num) {
     getElement(id).addEventListener("click", function(event) {
+        event.stopPropagation();
         copyCount++;
         const number = getString(num);
         const toCopy = number.toString();
@@ -38,30 +40,57 @@ function copyClicked(id, num) {
     });
 }
 
+function historyOfCall(txt, number) {
+    const text = getString(txt);
+    const number = getString(num);
+    let call = {
+       To: text,
+       Num: number,
+       Time: new Date().toLocaleTimeString()
+    };
+    callHistoryData.push(call);
+    console.log(call);
+}
+
+function historyDisplay() {
+    const HistoryDivHead = getElement("historyDiv");
+    historyDisplay.innerHTML = "";
+    for(const data of callHistoryData) {
+        const Div = document.createElement("div");
+        Div.innerHTML = `
+        <div class="w-full h-[75px] rounded-[8px] bg-[#FAFAFA] my-[10px] p-[10px]">
+            <div class="flex justify-between items-center">
+                    <div class="flex flex-col text-left items-start">
+                            <h2 class="font-bold text-[18px]">${data.To}</h2>
+                            <p class="text-[18px] text-[#5C5C5C]">${data.Num}</p>
+                    </div>
+                        <p class="text-[18px]">${data.Time}</p>
+            </div>
+        </div>
+        `;
+        HistoryDivHead.appendChild(Div);
+
+    }
+}
+
 function clickedCall(id, txt, num) {
-    getElement(id).addEventListener("click", function() {
+    getElement(id).addEventListener("click", function(event) {
+        event.stopPropagation();
         if (coinBalance >= 20) {
             const text = getString(txt);
-            const number = getNumber(num);
+            const number = getString(num);
             coinBalance -= 20;
             getElement("coinHave").innerText = coinBalance;
             alert(`📞 Calling ${text} ${number}...`);
+            historyOfCall(txt, number);
+            historyDisplay();
         } else {
             alert(`Balance Insufficient, You Must Have 20 Coin!`);
         }
     });
 }
 
-// CALLING FUNC
-function callButtons() { 
-    clickedCall("nationalEmergencyCall", "nationalEmergencyTxt", "nationalEmergencyNum");
-    clickedCall("policCall", "policTxt", "policNum");
-    clickedCall("fireCallBtn", "fireTxt", "fireNum");
-    clickedCall("ambulanceNumCallBtn", "ambulanceTxt", "ambulanceNum");
-    clickedCall("womenCallBtn", "womenTxt", "womenNum");
-    clickedCall("achCallBtn", "achTxt", "achNum");
-}
-// CALLING FUNC
+// CALLING FUNC OF HEARTS
 function heartButtons() {
     heartClicked("emerHeart");
     heartClicked("policHeart");
@@ -74,7 +103,7 @@ function heartButtons() {
     heartClicked("railHeart");
 }
 
-// CALLING FUNC 
+// CALLING FUNC OF COPIES
 function copyButtons() {
     copyClicked("emergencyCopyBtn", "nationalEmergencyNum"); 
     copyClicked("policCopyBtn", "policNum");
@@ -82,9 +111,25 @@ function copyButtons() {
     copyClicked("ambulanceCopyBtn", "ambulanceNum");
     copyClicked("womenCopyBtn", "womenNum");
     copyClicked("achCopyBtn", "achNum");
+    copyClicked("elecCopyBtn", "elecNum");
+    copyClicked("bracCopyBtn", "bracNum");
+    copyClicked("railCopyBtn", "railNum");
 }
 
-// STARTING MY SITE FUNC
+// CALLING FUNC OF CALL BTNs
+function callButtons() { 
+    clickedCall("nationalEmergencyCall", "nationalEmergencyTxt", "nationalEmergencyNum");
+    clickedCall("policCall", "policTxt", "policNum");
+    clickedCall("fireCallBtn", "fireTxt", "fireNum");
+    clickedCall("ambulanceNumCallBtn", "ambulanceTxt", "ambulanceNum");
+    clickedCall("womenCallBtn", "womenTxt", "womenNum");
+    clickedCall("achCallBtn", "achTxt", "achNum");
+    clickedCall("elecCallBtn", "elecTxt", "elecNum");
+    clickedCall("bracCallBtn", "bracTxt", "bracNum");
+    clickedCall("railCallBtn", "railTxt", "railNum");
+}
+
+// STARTING MY SITE's ACTIVITIES
 callButtons(); 
 heartButtons();
 copyButtons();
